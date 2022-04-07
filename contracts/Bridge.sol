1eb00067ebfb86d7ef8c78de0e1b831fb3b6556b
SamBorisov <samuil.bo5@gmail.com>
2022-04-07 13:56:52 +0300

    commit

---
+++ contracts/Bridge.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+
+contract Bridge {
+
+
+
+
+    
+}

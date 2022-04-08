ff44bac389873bb9d433c813d3869724aadb75f2
SamBorisov <samuil.bo5@gmail.com>
2022-04-08 10:45:37 +0300

    imports

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,8 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./IBridge.sol";
 

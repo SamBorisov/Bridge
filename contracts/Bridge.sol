f9bf8dbea4ea7cfe17ac87d6a1e38c55acaac841
Samuil <samuil.bo5@gmail.com>
2023-07-04 17:18:39 +0300

    Bridge V2

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2,95 +2,166 @@
 pragma solidity ^0.8.0;
 
 
-import "./Wrap.sol";
-
-
-contract Bridge is Wrap {
-
-    event Lock(uint8 targetChain, address token, uint256 amount, address receiver);
-
-       
-    event Unlock(uint8 sourceChain, address token, uint256 amount, address receiver);
-
-     
-    event Mint(uint8 sourceChain, uint256 amount, address receiver);
-
-     
-    event Burn(uint8 sourceChain, uint256 amount, address receiver);
-
-
-      address bridge;
-
-      uint8 _targetChain;
-      uint8 _sourceChain;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 
 
-    constructor (address _bridge){
-        bridge = _bridge;
+contract Bridge {
+
+
+    event Lock(uint8 indexed assetID, address token, uint256 amount, address receiver);
+
+    event Unlock(uint8 indexed assetID, address token, uint256 amount, address receiver);
+
+    event AssetAdded(uint16 indexed assetID,address token, bool mintable, bool bunrable);
+
+
+    address private bridge;
+    address private admin;
+
+
+    constructor (){
+        bridge = address(this);
+        admin = msg.sender;
     }
 
+    // Locking or Burining tokens
 
-    function lock(uint8 targetChain, address token, uint256 amount) external {
+    function lock(uint8 assetID, uint256 amount) external {
+
+        address token = tokenDetails[assetID].token;
+
         require(amount > 0,"Cannot Lock 0 tokens");
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        require(token != address(0),"Not supported token");
+        require(tokenDetails[assetID].assetID == assetID,"ID not found");
 
-        emit Lock(targetChain, address(token), amount, msg.sender);
+        if(tokenDetails[assetID].bunrable){
+            
+            ERC20Burnable _token = ERC20Burnable(token);
+            _token.burnFrom(msg.sender, amount);
+
+        } else {
+        IERC20 tokenInstance = IERC20(token);
+        require(tokenInstance.allowance(msg.sender, address(this)) >= amount, "Insufficient allowance");
+         tokenInstance.transferFrom(msg.sender, address(this), amount);
+        }
+
+        emit Lock(assetID, address(token), amount, msg.sender);
 
     }
 
+    // Unlocking or Minting tokens
 
-    function unlock(uint8 sourceChain, address token, uint256 amount, address receiver) external {
+    function unlock(uint8 assetID, uint256 amount, address receiver) external {
+
+        address token = tokenDetails[assetID].token;
+
         require(amount > 0,"Cannot Unlock 0 tokens");
         require(receiver == msg.sender,"Receiver should be the sender");
-        IERC20(token).transfer(msg.sender, amount);
-
-        emit Unlock(sourceChain, address(token), amount, msg.sender);
-
-    }
 
 
-    function mint(uint8 sourceChain, address token, uint256 amount, address receiver, string memory TokenName, string memory TokenSymbol) external {
-        require(amount > 0,"Cannot Mint 0 tokens");
-        require(receiver == msg.sender,"Receiver should be the sender");
+        if(tokenDetails[assetID].mintable){
 
-        address WrappedToken = orignalToWrap[token];
-        if (WrappedToken == address(0)) {
-            WrappedToken = wrapToken(sourceChain, token, TokenName, TokenSymbol);
+            ERC20PresetMinterPauser(token).mint(msg.sender, amount);
+
+        } else {
+            require(IERC20(token).balanceOf(address(this)) > amount,"The Bridge don't have enough tokes");
+            IERC20(token).approve(address(this), amount);
+            IERC20(token).transfer(msg.sender, amount);
         }
-        ERC20PresetMinterPauser(WrappedToken).mint(msg.sender, amount);
- //       IERC20(token).mint(receiver, amount);
 
-        emit Mint(sourceChain, amount, msg.sender);
+        emit Unlock(assetID, address(token), amount, msg.sender);
 
     }
 
 
-    function burn(uint8 sourceChain, address wrappedToken, uint256 amount, address receiver) external { 
-        require(amount > 0,"Cannot Burn 0 tokens");
-        require(receiver == msg.sender, "Receiver should be the sender");
-        require(wrappedDetails[wrappedToken].token != address(0),"Not supported token");
-        require(wrappedDetails[wrappedToken].sourceChain == sourceChain,"Bad source chain");
-
-        ERC20Burnable token = ERC20Burnable(wrappedToken);
-        token.burnFrom(receiver, amount);
-//        IERC20(token).burn(amount);
-
-        emit Burn(sourceChain, amount, msg.sender);
 
 
+
+    // adding and saving assets
+
+        mapping(uint8 => TokenInfo) internal tokenDetails;
+
+        struct TokenInfo {
+            string name;
+            string symbol;
+            uint8 assetID;
+            address token;
+            bool bunrable;
+            bool mintable;
+        }
+
+        uint8[] internal savedIDs;
+        TokenInfo[] public _TokensList;
+
+
+    function addAsset(uint8 assetID, address token, bool mintable, bool bunrable, string memory name, string memory symbol) external onlyAdmin  {
+
+        require(assetID > 0, "ID cannot be 0"); 
+        require(tokenDetails[assetID].token == address(0), "Token ID has address already");
+        require(checkSavedValues(assetID, token) == false ,"Token ID or address already set");
+
+
+        TokenInfo memory storeIt = TokenInfo({
+            name: name,
+            symbol: symbol,
+            assetID: assetID,
+            token: token,
+            bunrable: bunrable,
+            mintable: mintable
+        });
+
+        tokenDetails[assetID] = storeIt;
+        _TokensList.push(storeIt);
+        savedIDs.push(assetID);
+
+
+        emit AssetAdded(assetID, token, mintable, bunrable);
+   
     }
-        
 
 
 
+
+
+    // modifers for security
 
     modifier onlyBridge() {
         require(bridge == msg.sender, "only the bridge can trigger this function");
         _;
     } 
 
+    modifier onlyAdmin() {
+        require(admin == msg.sender, "only the admin can trigger this function");
+        _;
+    } 
 
+
+    // View functions
+
+    function viewAssets() public view returns (TokenInfo[] memory) {
+        return _TokensList;
+    }
+
+    function checkSavedValues(uint8 assetID, address token) internal view returns(bool) {
+
+        for (uint256 i = 0; i < savedIDs.length; i++) {
+            if (tokenDetails[savedIDs[i]].assetID == assetID || tokenDetails[savedIDs[i]].token == token) {
+               return true;
+            }
+        }
+        return false;
+    }
+
+    function getBalanceOfToken(uint8 assetID) public view returns(uint256) {
+
+        require(tokenDetails[assetID].token != address(0),"Not supported token");
+        require(tokenDetails[assetID].assetID == assetID,"ID not found");
+
+        return IERC20(tokenDetails[assetID].token).balanceOf(msg.sender);
+        
+    }
     
  
 }
--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -14,5 +12,9 @@ contract TestToken is ERC20, Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }

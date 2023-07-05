ce2a5b1b266ed0e75039d65d0c9136f1160779fe
Samuil <samuil.bo5@gmail.com>
2023-07-05 13:01:40 +0300

    simplifying and removing unnecessary variables || function

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2,52 +2,45 @@
 pragma solidity ^0.8.0;
 
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 
 
-contract Bridge {
+contract Bridge is Ownable{
 
 
-    event Lock(uint8 indexed assetID, address token, uint256 amount, address receiver);
+    event Lock(uint8 indexed assetID, address indexed token, uint256 amount, address user, address receiver);
 
-    event Unlock(uint8 indexed assetID, address token, uint256 amount, address receiver);
+    event Unlock(uint8 indexed assetID, address indexed token, uint256 amount, address user, address receiver);
 
-    event AssetAdded(uint16 indexed assetID,address token, bool mintable, bool bunrable);
-
-
-    address private bridge;
-    address private admin;
-
-
-    constructor (){
-        bridge = address(this);
-        admin = msg.sender;
-    }
 
     // Locking or Burining tokens
 
-    function lock(uint8 assetID, uint256 amount) external {
+    function lock(uint8 assetID, uint256 amount, address receiver) external {
 
         address token = tokenDetails[assetID].token;
 
         require(amount > 0,"Cannot Lock 0 tokens");
         require(token != address(0),"Not supported token");
         require(tokenDetails[assetID].assetID == assetID,"ID not found");
+        require(receiver != address(0),"The receiver shoud be a valid address");
 
-        if(tokenDetails[assetID].bunrable){
-            
-            ERC20Burnable _token = ERC20Burnable(token);
-            _token.burnFrom(msg.sender, amount);
+        if(tokenDetails[assetID].wrapped){
+
+            require(IERC20(token).allowance(msg.sender, address(this)) >= amount, "Insufficient allowance");
+            require(IERC20(token).balanceOf(msg.sender) > amount,"Insufficient balance");
+            ERC20PresetMinterPauser(token).burnFrom(msg.sender, amount);
 
         } else {
-        IERC20 tokenInstance = IERC20(token);
-        require(tokenInstance.allowance(msg.sender, address(this)) >= amount, "Insufficient allowance");
-         tokenInstance.transferFrom(msg.sender, address(this), amount);
+
+            require(IERC20(token).allowance(msg.sender, address(this)) >= amount, "Insufficient allowance");
+            require(IERC20(token).balanceOf(msg.sender) >= amount, "Insufficient balance");
+            IERC20(token).transferFrom(msg.sender, address(this), amount);
         }
 
-        emit Lock(assetID, address(token), amount, msg.sender);
+        emit Lock(assetID, address(token), amount, msg.sender, receiver);
 
     }
 
@@ -58,45 +51,42 @@ contract Bridge {
         address token = tokenDetails[assetID].token;
 
         require(amount > 0,"Cannot Unlock 0 tokens");
-        require(receiver == msg.sender,"Receiver should be the sender");
+        require(token != address(0),"Not supported token");
+        require(tokenDetails[assetID].assetID == assetID,"ID not found");
+        require(receiver != address(0),"The receiver shoud be a valid address");
 
 
-        if(tokenDetails[assetID].mintable){
+        if(tokenDetails[assetID].wrapped){
 
-            ERC20PresetMinterPauser(token).mint(msg.sender, amount);
+            ERC20PresetMinterPauser(token).mint(receiver, amount);
 
         } else {
             require(IERC20(token).balanceOf(address(this)) > amount,"The Bridge don't have enough tokes");
-            IERC20(token).approve(address(this), amount);
-            IERC20(token).transfer(msg.sender, amount);
+            IERC20(token).transfer(receiver, amount);
         }
 
-        emit Unlock(assetID, address(token), amount, msg.sender);
+        emit Unlock(assetID, address(token), amount, msg.sender, receiver);
 
     }
 
 
 
+    // Adding and Saving tokens
 
+    event AssetAdded(uint16 indexed assetID,address indexed token, bool wrapped);
 
-    // adding and saving assets
-
-        mapping(uint8 => TokenInfo) internal tokenDetails;
+        mapping(uint8 => TokenInfo) public tokenDetails;
 
         struct TokenInfo {
-            string name;
-            string symbol;
             uint8 assetID;
             address token;
-            bool bunrable;
-            bool mintable;
+            bool wrapped;
         }
 
         uint8[] internal savedIDs;
-        TokenInfo[] public _TokensList;
 
 
-    function addAsset(uint8 assetID, address token, bool mintable, bool bunrable, string memory name, string memory symbol) external onlyAdmin  {
+    function addAsset(uint8 assetID, address token, bool wrapped) external onlyOwner {
 
         require(assetID > 0, "ID cannot be 0"); 
         require(tokenDetails[assetID].token == address(0), "Token ID has address already");
@@ -104,45 +94,22 @@ contract Bridge {
 
 
         TokenInfo memory storeIt = TokenInfo({
-            name: name,
-            symbol: symbol,
             assetID: assetID,
             token: token,
-            bunrable: bunrable,
-            mintable: mintable
+            wrapped: wrapped
         });
 
         tokenDetails[assetID] = storeIt;
-        _TokensList.push(storeIt);
         savedIDs.push(assetID);
 
 
-        emit AssetAdded(assetID, token, mintable, bunrable);
+        emit AssetAdded(assetID, token, wrapped);
    
     }
 
 
-
-
-
-    // modifers for security
-
-    modifier onlyBridge() {
-        require(bridge == msg.sender, "only the bridge can trigger this function");
-        _;
-    } 
-
-    modifier onlyAdmin() {
-        require(admin == msg.sender, "only the admin can trigger this function");
-        _;
-    } 
-
-
     // View functions
 
-    function viewAssets() public view returns (TokenInfo[] memory) {
-        return _TokensList;
-    }
 
     function checkSavedValues(uint8 assetID, address token) internal view returns(bool) {
 
@@ -154,14 +121,5 @@ contract Bridge {
         return false;
     }
 
-    function getBalanceOfToken(uint8 assetID) public view returns(uint256) {
-
-        require(tokenDetails[assetID].token != address(0),"Not supported token");
-        require(tokenDetails[assetID].assetID == assetID,"ID not found");
-
-        return IERC20(tokenDetails[assetID].token).balanceOf(msg.sender);
-        
-    }
-    
  
 }
